4d2ffe813cb704530b6074985479d0e8519c6ddd
Andrei Boaca <dreilex@Andreis-MacBook-Pro.local>
2024-12-19 10:10:01 +0100

    Implemented toDoInput to collect the input from the user and display it

--- src/ToDoInput.jsx
+++ src/ToDoInput.jsx
@@ -1,8 +1,19 @@
-function ToDoInput () {
+import App from "./App";
+import { useState } from "react";
+
+function ToDoInput ({addTask}) {
+  const [val, setVal] = useState("");
+  const addInput = () => {
+    addTask(val);
+    setVal("");
+  }
+  function handleChange(e) {
+    setVal(e.target.value);
+  }
     return(
       <div className = "userInput">
-      <input id ="addInput" type="text" name="myInput" placeholder='Add a task...'/>
-      <button id ="submitInput" type="submit">Add</button>
+      <input id ="addInput" type="text" name="myInput" placeholder='Add a task...' value ={val} onChange={handleChange}/>
+      <button id ="submitInput" type="submit" onClick={addInput}>Add</button>
     </div>
     )
   };
