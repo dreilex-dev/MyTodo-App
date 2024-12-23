f832aed6acb295a3ee977c53fca1de42305ce1ce
Andrei Boaca <adi.boaca@gmail.com>
2024-12-23 06:31:48 +0200

    Place the checkbox before the paragraph

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -5,11 +5,11 @@ function ToDoList({ tasks }) {
         {tasks.map((task, index) => (
           <li className="list-group-item" key={index} style={{ minWidth: "160px", color: "#fdf2e9" }}>
             <div className="align-items-center row">
+            <div className="text-end col-2">
+                <input type="checkbox" className="form-check-input" />
+              </div>
               <div className="col-10" style={{minWidth: "200px"}}>
                 <p className="fw-bold text-muted no-margin">{task}</p>
-              </div>
-              <div className="text-end col-2">
-                <input type="checkbox" className="form-check-input" />
               </div>
             </div>
           </li>
