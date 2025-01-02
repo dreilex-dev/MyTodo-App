1bd1f8c14b211af6ae6d51aba42de8d758b31971
Andrei Boaca <adi.boaca@gmail.com>
2025-01-02 14:38:31 +0200

    Style: delete button added. Pending functionality.

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,23 +1,30 @@
-function ToDoList({ tasks , onToggleTask}) {
-  
+function ToDoList({ tasks, onToggleTask }) {
   return (
     <div className="row mt-2" style={{ margin: "auto" }} id="taskList">
       <ul className="list-group">
         {tasks.map((task, index) => (
-          <li className="list-group-item" key={index} style={{ minWidth: "160px", color: "#fdf2e9" }}>
-            <div className="align-items-center row">
-            <div className="text-end col-2">
-                <input 
-                id={task.id} 
-                type="checkbox" 
-                className="form-check-input" 
-                onChange={() => onToggleTask(task.id)}                
-                />
-              </div>
-              <div className="col-10" style={{minWidth: "200px"}}>
-                <p className="fw-bold text-muted no-margin">{task.name}</p>
-              </div>
+          <li
+            className="list-group-item d-flex justify-content-between align-items-center"
+            key={index}
+            style={{ minWidth: "160px", color: "#fdf2e9" }}
+          >
+            <div className="d-flex align-items-center">
+              <input
+                id={task.id}
+                type="checkbox"
+                className="form-check-input me-2"
+                onChange={() => onToggleTask(task.id)}
+              />
+              <p className="fw-bold text-muted mb-0 me-2">{task.name}</p>
             </div>
+            <button
+              className="btn btn-danger btn-sm"
+              id="deleteButton"
+              type="button"
+              style={{ borderRadius: "6px" }}
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
